1ad8789b594e1b549bfe00bda7ae9d98695d76da
Mateus Barros <mateusverissimo94@gmail.com>
2024-07-16 21:11:06 -0300

    first commit

---
+++ src/main/java/me/dio/SantanderBootcamp2024Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBootcamp2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBootcamp2024Application.class, args);
+	}
+
+}
